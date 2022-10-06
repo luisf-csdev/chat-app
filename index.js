a96619064bf9ef47ede1b883437e94347dbfb50c
Luís Felipe <luisf.csdev@gmail.com>
2022-10-06 00:35:21 -0300

    first messages

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const socketIO = require('socket.io');
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log("Server running");
+})
+
+const messages = []
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+
+    console.log('new connection');
+    socket.emit('update_messages', messages);
+
+    socket.on('new_message', (data) => {
+        messages.push(data.msg);
+
+        io.emit('update_messages', messages);
+    })
+
+
 })
