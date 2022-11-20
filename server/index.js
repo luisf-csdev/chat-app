69589c92bcaafce9fe0ead96bda31e664bbea3cf
Luís Felipe <luisf.csdev@gmail.com>
2022-11-19 23:08:57 -0300

    modify cors origin

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const { Server } = require('socket.io');
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://chat-rooms-socket.netlify.app/',
         methods: ['GET', 'POST']
     }
 });
