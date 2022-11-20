ff8fc2fdea435aa04349fbfc3da85ed1dcd32db0
Luís Felipe <luisf.csdev@gmail.com>
2022-11-19 21:46:54 -0300

    client branch

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import io from 'socket.io-client'
 import { useState } from 'react'
 import Chat from './components/Chat';
+const ENDPOINT = 'https://chat-rooms-server.fly.dev/'
 
-const socket = io.connect('http://localhost:3001');
+const socket = io.connect(ENDPOINT);
 
 
 function App() {
